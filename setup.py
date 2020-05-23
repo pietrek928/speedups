29a651a4cae69942f101648ebbcc4856ac21ea8e
Pietrek xD <pietrek.j@gmail.com>
2020-05-23 20:45:28 +0200

    rename

--- setup.py
+++ setup.py
@@ -3,16 +3,16 @@ from setuptools import setup, find_packages, Extension
 
 optim_module = Extension(
     'optim',
-    sources=['speedutils/python.cc'],
+    sources=['speedups/python.cc'],
     libraries=['boost_python38'],
     language='c++',
     extra_compile_args=["-std=c++17", "-O3"]
 )
 
 setup(
-    name='speedutils',
+    name='speedups',
     packages=find_packages(),
-    ext_package='speedutils',
+    ext_package='speedups',
     ext_modules=cythonize([
         optim_module
     ])
